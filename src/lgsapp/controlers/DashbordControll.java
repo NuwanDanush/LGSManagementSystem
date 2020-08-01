085458b2b87e3962ad12e87554e4e3847b31702e
NuwanDanush <danushkanuwan252@gmail.com>
2020-08-01 22:41:28 +0530

    DashboardController

--- src/lgsapp/controlers/DashbordControll.java
+++ src/lgsapp/controlers/DashbordControll.java
@@ -24,4 +24,34 @@ public class DashbordControll implements Initializable {
 
     @FXML
     private Button btn_help;
+
+
+    //my bad - the freaking mouse event
+  /*  @FXML
+    private void handleButtonClicks(javafx.event.ActionEvent mouseEvent) {
+        if (mouseEvent.getSource() == btnDashboard) {
+            loadStage("/lgsapp/views/home.fxml");
+        } else if (mouseEvent.getSource() == btnAddSec) {
+          //  loadStage("/home/fxml/Students.fxml");
+        } else if (mouseEvent.getSource() == btn_help) {
+           // loadStage("/home/fxml/Timetable.fxml");
+        }
+    }*/
+
+    @FXML
+    void btnHome(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/lgsapp/views/home.fxml"));
+
+        Node node = (Node) event.getSource();
+
+        Stage stage = (Stage) node.getScene().getWindow();
+
+        stage.setScene(new Scene(root));
+
+        stage.setFullScreen (true);
+
+
+
+    }
+
 }
