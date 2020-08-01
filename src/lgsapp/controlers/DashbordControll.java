8088396c848caea9bd76328cfe4f228543f2c6b2
NuwanDanush <danushkanuwan252@gmail.com>
2020-08-01 22:43:11 +0530

    DashboardController

--- src/lgsapp/controlers/DashbordControll.java
+++ src/lgsapp/controlers/DashbordControll.java
@@ -72,13 +72,46 @@ public class DashbordControll implements Initializable {
             stage.setScene(new Scene(root));
     
             //stage.setFullScreen(true);
-    
-    
-    
-    
-        }
-    
 
+        }
+    }
+
+
+    double x = 0, y =0;
+    @FXML
+    void pressed(MouseEvent event) {
+        x = event.getSceneX();
+        y = event.getSceneY();
+    }
+
+    @FXML
+    void dragged(MouseEvent event) {
+
+        Node node = (Node) event.getSource();
+
+        Stage stage = (Stage) node.getScene().getWindow();
+
+        stage.setX(event.getScreenX() - x);
+        stage.setY(event.getScreenY() - y);
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    private void loadStage(String fxml) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(fxml));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.getIcons().add(new Image("/home/icons/icon.png"));
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
