5062a5df6906be3d2a872cb7fdaa7b7a2aa3d1a4
NuwanDanush <danushkanuwan252@gmail.com>
2020-08-01 22:51:23 +0530

    HomeController

--- src/lgsapp/controlers/HomeController.java
+++ src/lgsapp/controlers/HomeController.java
@@ -33,3 +33,140 @@ import java.time.LocalDate;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.function.Predicate;
+
+public class HomeController implements Initializable{
+    private File selectedFile =null;
+    private FileInputStream fis;
+
+    Connection con = DbConnect.getConnection();
+
+    private FileChooser fileChooser;
+    private Desktop desktop = Desktop.getDesktop();
+    private String imageFile;
+
+    //define row clicked variables
+    String clickfname;
+    String clicklname;
+    String clickoffice;
+    String clicksrchyr;
+
+    Secratary secratary;
+
+    @FXML
+    private TextField txtFirstname;
+
+    @FXML
+    private TextField txtLastname;
+
+    @FXML
+    private TextField txtContact;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtWOP;
+
+    @FXML
+    private ComboBox cmbGender;
+
+    @FXML
+    private ComboBox cmbCuryr;
+
+    @FXML
+    private ComboBox cmbOffice;
+
+    @FXML
+    private DatePicker txtBirthday;
+
+    @FXML
+    private DatePicker txtFirstAppdate;
+
+    @FXML
+    private DatePicker txtUpgrading;
+
+    @FXML
+    private DatePicker txtRetirement;
+
+    @FXML
+    private DatePicker txtIncrement;
+
+    @FXML
+    private CheckBox chkPb;
+
+    @FXML
+    private CheckBox chkPm;
+
+    @FXML
+    private CheckBox chkPe;
+
+    @FXML
+    private RadioButton radioyes;
+
+    @FXML
+    private RadioButton radiono;
+
+    @FXML
+    private TableView<Secratary> tblData;
+
+    @FXML
+    private TableColumn<Secratary, String> colfname;
+
+    @FXML
+    private TableColumn<Secratary, String> collname;
+
+    @FXML
+    private TableColumn<Secratary, String> colwop;
+
+    @FXML
+    private TableColumn<Secratary, String> coloffice;
+
+    @FXML
+    private TableColumn<Secratary, String> colcontact;
+
+    @FXML
+    private TableColumn<Secratary, String> colbday;
+
+    @FXML
+    private TableColumn<Secratary, String> colfappdate;
+
+    @FXML
+    private TableColumn<Secratary, String> colupgdate;
+
+    @FXML
+    private TableColumn<Secratary, String> colretdate;
+
+    @FXML
+    private TableColumn<Secratary, String> colincdate;
+
+    @FXML
+    private TableColumn<Secratary, String> colinc;
+
+    @FXML
+    private TableColumn<Secratary, String> colbeg;
+
+    @FXML
+    private TableColumn<Secratary, String> colmid;
+
+    @FXML
+    private TableColumn<Secratary, String> colend;
+
+    @FXML
+    private TextField txtSearch;
+
+    @FXML
+    private TextField txtsearchyr;
+
+
+
+    ObservableList<Secratary> oblist = FXCollections.observableArrayList(); //get data from model
+
+
+
+
+    @FXML
+    private ImageView img_frame;
+
+
+
+}
