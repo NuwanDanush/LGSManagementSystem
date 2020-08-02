94dc4b003f5baa6ec3eefbdfd4fb45f52d469930
NuwanDanush <danushkanuwan252@gmail.com>
2020-08-02 19:48:13 +0530

    SignupController

--- src/lgsapp/controlers/SignupController.java
+++ src/lgsapp/controlers/SignupController.java
@@ -60,4 +60,136 @@ public class SignupController implements Initializable {
         stage.setScene(new Scene(root));
         stage.setFullScreen (true);
     }
+    @FXML
+    void signup(MouseEvent event) {
+
+        String username = txt_uname.getText();
+        String email = txt_email.getText();
+        String password = txt_password.getText();
+
+        boolean emailvalid = isValidEmailAddress(email); //for valid email address
+        boolean valid = true;
+
+        if(username.isEmpty()){
+            valid = false;
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Can't change your account details");
+            String s = "The Username is empty";
+            alert.setContentText(s);
+            alert.showAndWait();
+
+
+        }
+        else if(email.isEmpty()){
+            valid = false;
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Can't change your account details");
+            String s = "The Email is empty";
+            alert.setContentText(s);
+            alert.showAndWait();
+
+        }
+
+        else if(!emailvalid){
+            valid = false;
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Can't change your account details");
+            String s = "The Email is not valid";
+            alert.setContentText(s);
+            alert.showAndWait();
+
+        }
+        else if(password.isEmpty()){
+            valid = false;
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Can't change your account details");
+            String s = "The Password is empty";
+            alert.setContentText(s);
+            alert.showAndWait();
+
+        }
+        else if(password.length()<8){
+            valid = false;
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Can't change your account details");
+            String s = "The Password must be at least 8 characters ";
+            alert.setContentText(s);
+            alert.showAndWait();
+
+        }
+
+        else if(password.length()>20){
+            valid = false;
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Can't change your account details");
+            String s = "The Password is too long ";
+            alert.setContentText(s);
+            alert.showAndWait();
+
+        }
+
+        else {
+
+
+
+            try {
+
+
+                PreparedStatement ps = null;
+
+                String query = "UPDATE `users` SET `username`=?,`email`=?,`password`=?  WHERE `username`=? ";
+
+
+
+                ps = con.prepareStatement(query);
+                ps.setString(1, username);
+                ps.setString(2, email);
+                ps.setString(3, password);
+
+                String myuname = LoginController.getMyusername();
+                ps.setString(4, myuname);
+
+
+
+                if (ps.executeUpdate() > 0) {
+                    System.out.println("user registered");
+
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Change Your Account details");
+                    String s = "Successfully Change Your Account details";
+                    alert.setContentText(s);
+                    alert.showAndWait();
+
+                    txt_uname.setText("");
+                    txt_email.setText("");
+                    txt_password.setText("");
+
+
+                   //load dashboard
+                    Parent root = FXMLLoader.load(getClass().getResource("/lgsapp/views/dashbord.fxml"));
+                    Node node = (Node) event.getSource();
+                    Stage stage = (Stage) node.getScene().getWindow();
+                    stage.setScene(new Scene(root));
+                    stage.setFullScreen (true);
+
+
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                valid = false;
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Can't change your account details");
+                String s = "Please re Login and try..";
+                alert.setContentText(s);
+                alert.showAndWait();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+    }
+
 }
