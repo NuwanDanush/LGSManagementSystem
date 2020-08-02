cdd5ca447b972dfd420148c07659328a68e5c970
NuwanDanush <danushkanuwan252@gmail.com>
2020-08-02 19:34:42 +0530

    LoginController

--- src/lgsapp/controlers/LoginController.java
+++ src/lgsapp/controlers/LoginController.java
@@ -26,4 +26,67 @@ public class LoginController implements Initializable{
     @FXML
     private PasswordField pf_password;
 
+    @FXML
+    void btnlogin(MouseEvent event) throws SQLException, IOException {
+
+
+        String username, password;
+        PreparedStatement ps;
+
+        username = tf_username.getText();
+        password = pf_password.getText();
+
+        myusername = username;
+
+        Connection con = DbConnect.getConnection();
+
+       try {
+           ps = con.prepareStatement("SELECT * FROM users WHERE username=? AND password =?");
+           ps.setString(1,username);
+           ps.setString(2, password);
+           ResultSet rs = ps.executeQuery();
+
+           String msg = "Text saved: ";
+           
+           if (rs.next()) {
+               System.out.print("Login Sucess");
+               Parent root = FXMLLoader.load(getClass().getResource("/lgsapp/views/dashbord.fxml"));
+
+               Node node = (Node) event.getSource();
+
+               Stage stage = (Stage) node.getScene().getWindow();
+
+               stage.setScene(new Scene(root));
+
+               stage.setFullScreen (true);
+
+           } else {
+
+
+               System.out.print("Login fail");
+
+               //Add dialog box
+
+               Alert alert = new Alert(Alert.AlertType.ERROR);
+               alert.setTitle("Login Error");
+               String s = "Enter The valid user name or password ";
+               alert.setContentText(s);
+               alert.showAndWait();
+
+
+
+
+
+
+
+       }
+
+
+       } catch (Exception e) {
+           System.out.println("Login fail with sqlite error");
+           System.out.println(e);
+       }
+   }
+
+
 }
