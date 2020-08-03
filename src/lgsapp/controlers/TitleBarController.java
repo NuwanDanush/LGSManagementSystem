f6003202b9b2d074cd0d0e761dcc2fa19acb6c9a
NuwanDanush <danushkanuwan252@gmail.com>
2020-08-03 19:17:46 +0530

    TitleBarController

--- src/lgsapp/controlers/TitleBarController.java
+++ src/lgsapp/controlers/TitleBarController.java
@@ -12,6 +12,32 @@ import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
-public class TitleBarController implements Initializable{
-    
+public class TitleBarController implements Initializable {
+    @FXML
+    void close(MouseEvent event) {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION); // confirmation alert
+        alert.setTitle("Exit");
+        String s = "Are You Sure Want to exit..!";
+        alert.setContentText(s);
+        Optional<ButtonType> result = alert.showAndWait();
+        if ((result.isPresent()) && (result.get() == ButtonType.OK)) {
+
+            Node node = (Node) event.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
+            stage.close();
+        }
+
+    }
+
+    @FXML
+    void max(MouseEvent event) {
+        Node node = (Node) event.getSource();
+
+        Stage stage = (Stage) node.getScene().getWindow();
+
+        stage.setFullScreenExitHint(" ");
+        stage.setFullScreen(true);
+
+    }
 }
