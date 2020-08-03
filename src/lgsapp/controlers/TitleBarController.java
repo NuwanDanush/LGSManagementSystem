4a1bd1af2d49beb0db970079e11c0d7fd0632723
NuwanDanush <danushkanuwan252@gmail.com>
2020-08-03 19:17:03 +0530

    TitleBarController

---
+++ src/lgsapp/controlers/TitleBarController.java
@@ -0,0 +1,17 @@
+package lgsapp.controlers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+public class TitleBarController implements Initializable{
+    
+}
