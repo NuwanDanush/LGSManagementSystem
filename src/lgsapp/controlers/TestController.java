9c746af32b578629f6a89e5cbc9de79179578947
NuwanDanush <danushkanuwan252@gmail.com>
2020-08-03 19:12:18 +0530

    TestController

---
+++ src/lgsapp/controlers/TestController.java
@@ -0,0 +1,18 @@
+package lgsapp.controlers;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
