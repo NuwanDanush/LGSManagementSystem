f5e1b2c4909fa2cae6bac17e7d2f59fee70659e3
NuwanDanush <danushkanuwan252@gmail.com>
2020-08-03 19:13:37 +0530

    TestController

--- src/lgsapp/controlers/TestController.java
+++ src/lgsapp/controlers/TestController.java
@@ -16,3 +16,57 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+
+public final class TestController extends Application {
+    private Desktop desktop = Desktop.getDesktop();
+
+    @Override
+    public void start(final Stage stage) {
+        stage.setTitle("File Chooser Sample");
+
+        final FileChooser fileChooser = new FileChooser();
+        final Button openButton = new Button("Open a Picture...");
+        final Button openMultipleButton = new Button("Open Pictures...");
+        MenuItem cmItem2 = new MenuItem("Save Image");
+
+        openButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(final ActionEvent e) {
+                configureFileChooser(fileChooser);
+                File file = fileChooser.showOpenDialog(stage);
+                if (file != null) {
+                    openFile(file);
+                }
+            }
+        });
+
+        openMultipleButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(final ActionEvent e) {
+                configureFileChooser(fileChooser);
+                List<File> list = fileChooser.showOpenMultipleDialog(stage);
+                if (list != null) {
+                    for (File file : list) {
+                        openFile(file);
+                    }
+                }
+            }
+        });
+
+        final GridPane inputGridPane = new GridPane();
+
+        GridPane.setConstraints(openButton, 0, 1);
+        GridPane.setConstraints(openMultipleButton, 1, 1);
+        inputGridPane.setHgap(6);
+        inputGridPane.setVgap(6);
+        inputGridPane.getChildren().addAll(openButton, openMultipleButton);
+
+        final Pane rootGroup = new VBox(12);
+        rootGroup.getChildren().addAll(inputGridPane);
+        rootGroup.setPadding(new Insets(12, 12, 12, 12));
+
+        stage.setScene(new Scene(rootGroup));
+        stage.show();
+    }
+
+}
