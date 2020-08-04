e9bab3f7a77106230dc0f4bbaebc2289d65735d4
NuwanDanush <danushkanuwan252@gmail.com>
2020-08-04 11:08:31 +0530

    DBconnect

---
+++ src/lgsapp/helpers/DbConnect.java
@@ -0,0 +1,12 @@
+package lgsapp.helpers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DbConnect{
+    private DbConnect() {
+
+    }
+}
