a49c239b7983a404fe620a9af6a0a7ba8e81548a
NuwanDanush <danushkanuwan252@gmail.com>
2020-08-04 11:10:32 +0530

    DBconnect

--- src/lgsapp/helpers/DbConnect.java
+++ src/lgsapp/helpers/DbConnect.java
@@ -15,5 +15,22 @@ public class DbConnect{
     }
     public static Connection getConnection(){
         String connect_string = "jdbc:sqlite:lgsdb.sqlite";
+        Connection connection = null;
+        try {
+
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection(connect_string);
+            System.out.println("Connection Sucesss......................");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("not connected.....");
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("not connected");
+        }
+
+        return connection;
+    }
     }
 }
