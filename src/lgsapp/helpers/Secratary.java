ea3d960c1e773513a5fae04710896c74ff7fc655
NuwanDanush <danushkanuwan252@gmail.com>
2020-08-04 11:15:56 +0530

    Secratary

--- src/lgsapp/helpers/Secratary.java
+++ src/lgsapp/helpers/Secratary.java
@@ -16,4 +16,25 @@ public class Secratary{
     String beg;
     String mid;
     String end ;
+    
+    public Secratary(String fname, String lname, String wop, String office, String contact, String bday, String fappdate, String upgradedate, String retdate, String incdate, String inc, String beg, String mid, String end) {
+        this.fname = fname;
+        this.lname = lname;
+        this.wop = wop;
+        this.office = office;
+        this.contact = contact;
+        this.gender = gender;
+        this.bday = bday;
+        this.fappdate = fappdate;
+        this.upgradedate = upgradedate;
+        this.retdate = retdate;
+        this.incdate = incdate;
+        this.inc = inc;
+        this.beg = beg;
+        this.mid = mid;
+        this.end = end;
+    }
+
+
+
 }
