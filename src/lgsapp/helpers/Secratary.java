d173baae59931c90092281b51e9000af2148292f
NuwanDanush <danushkanuwan252@gmail.com>
2020-08-04 11:14:38 +0530

    Secratary

--- src/lgsapp/helpers/Secratary.java
+++ src/lgsapp/helpers/Secratary.java
@@ -1,5 +1,19 @@
 package lgsapp.helpers;
 
 public class Secratary{
-    
+    String fname;
+    String lname;
+    String wop;
+    String office;
+    String contact;
+    String gender;
+    String bday;
+    String fappdate;
+    String upgradedate;
+    String retdate;
+    String incdate;
+    String inc;
+    String beg;
+    String mid;
+    String end ;
 }
